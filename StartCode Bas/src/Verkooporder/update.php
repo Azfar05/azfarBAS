b33d3898aa0401bc67d1f2bfeedbc7404d2864ba
Azfar05 <112875863+Azfar05@users.noreply.github.com>
2024-06-20 23:01:44 +0200

    Alleen nog TeSTEN tOEvoEgEN

--- StartCode Bas/src/Verkooporder/update.php
+++ StartCode Bas/src/Verkooporder/update.php
@@ -1,24 +1,50 @@
 <?php
     // auteur: azfar
-    // functie: update class Artikel
+    // functie: update class VerkoopOrder
 
     // Autoloader classes via composer
     require '../../vendor/autoload.php';
+    use Bas\classes\VerkoopOrder;
+    use Bas\classes\Klant;
     use Bas\classes\Artikel;
-    
+
+    $verkoopOrder = new VerkoopOrder;
+    $klant = new Klant;
     $artikel = new Artikel;
 
+    // Code voor een update
     if(isset($_POST["update"]) && $_POST["update"] == "Wijzigen"){
+        $verkOrdId = $_POST["verkOrdId"]; // Get ID van input
+        $klantId = $_POST["klantId"]; 
+        $artId = $_POST["artId"]; 
+        $verkOrdDatum = $_POST["verkOrdDatum"]; 
+        $verkOrdBestAantal = $_POST["verkOrdBestAantal"];
+        $verkOrdStatus = $_POST["verkOrdStatus"];
 
-        // Code voor een update
+        $row = array(
+            "verkOrdId" => $verkOrdId,
+            "klantId" => $klantId,
+            "artId" => $artId,
+            "verkOrdDatum" => $verkOrdDatum,
+            "verkOrdBestAantal" => $verkOrdBestAantal,
+            "verkOrdStatus" => $verkOrdStatus
+        );
         
+        $success = $verkoopOrder->updateVerkoopOrder($row); 
+        
+        if ($success) {
+            echo "Verkooporder succesvol bijgewerkt.";
+        } else {
+            echo "Fout bij het bijwerken van verkooporder.";
+        }
     }
 
-    if (isset($_GET['artId'])){
-        $row = $artikel->getArtikel($_GET['artId']);
-
-
+    if (isset($_GET['verkOrdId'])){
+        $row = $verkoopOrder->getVerkoopOrder($_GET['verkOrdId']);
+        $klanten = $klant->getKlanten(); // Assuming this method exists
+        $artikelen = $artikel->getArtikelen(); // Assuming this method exists
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,15 +54,40 @@
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-<h1>CRUD Artikel</h1>
+<h1>CRUD VerkoopOrder</h1>
 <h2>Wijzigen</h2>    
 <form method="post">
-<input type="hidden" name="artId" 
-    value="<?php if(isset($row)) { echo $row['artId']; } ?>">
-<input type="text" name="artOmschrijving" required 
-    value="<?php if(isset($row)) {echo $row['artOmschrijving']; }?>"> *</br>
-<input type="text" name="artInkoop" required 
-    value="<?php if(isset($row)) {echo $row["artInkoop"]; }?>"> *</br></br>
+<input type="hidden" name="verkOrdId" value="<?php echo isset($row['verkOrdId']) ? $row['verkOrdId'] : ''; ?>">
+
+<label for="klantId">Klant:</label>
+<select name="klantId" required>
+    <?php
+        foreach ($klanten as $klant) {
+            $selected = $klant['klantId'] == $row['klantId'] ? 'selected' : '';
+            echo "<option value='{$klant['klantId']}' $selected>{$klant['klantNaam']}</option>";
+        }
+    ?>
+</select> *</br>
+
+<label for="artId">Artikel:</label>
+<select name="artId" required>
+    <?php
+        foreach ($artikelen as $artikel) {
+            $selected = $artikel['artId'] == $row['artId'] ? 'selected' : '';
+            echo "<option value='{$artikel['artId']}' $selected>{$artikel['artOmschrijving']}</option>";
+        }
+    ?>
+</select> *</br>
+
+<label for="verkOrdDatum">Datum:</label>
+<input type="text" name="verkOrdDatum" required value="<?php echo isset($row['verkOrdDatum']) ? $row['verkOrdDatum'] : ''; ?>"> *</br>
+
+<label for="verkOrdBestAantal">Bestel Aantal:</label>
+<input type="text" name="verkOrdBestAantal" required value="<?php echo isset($row['verkOrdBestAantal']) ? $row['verkOrdBestAantal'] : ''; ?>"> *</br>
+
+<label for="verkOrdStatus">Status:</label>
+<input type="text" name="verkOrdStatus" required value="<?php echo isset($row['verkOrdStatus']) ? $row['verkOrdStatus'] : ''; ?>"> *</br>
+
 <input type="submit" name="update" value="Wijzigen">
 </form></br>
 
@@ -47,6 +98,6 @@
 
 <?php
     } else {
-        echo "Geen artId opgegeven<br>";
+        echo "Geen verkOrdId opgegeven<br>";
     }
 ?>
